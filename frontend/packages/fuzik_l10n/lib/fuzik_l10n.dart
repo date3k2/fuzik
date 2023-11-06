0ccbf922c51b8216a2240fb70077f1a1faa2f691
sclearner <71807587+sclearner@users.noreply.github.com>
2023-11-06 18:50:47 +0700

    Goodbye lib ignore

---
+++ frontend/packages/fuzik_l10n/lib/fuzik_l10n.dart
@@ -0,0 +1,7 @@
+library fuzik_l10n;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ frontend/packages/taxonomy/lib/taxonomy.dart
@@ -0,0 +1,7 @@
+library taxonomy;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
